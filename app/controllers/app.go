a77c084bff2afa98cbbe0f5befab18b191c9bb8f
mmcdaris <morganmcdaris@yahoo.com>
2014-02-23 23:58:22 -0800

    add simple form to finish off the revel tutorial

--- app/controllers/app.go
+++ app/controllers/app.go
@@ -7,6 +7,19 @@ type App struct {
 }
 
 func (c App) Index() revel.Result {
-	greeting := "Hello From the app!"
+	greeting := "The History of Life"
   return c.Render(greeting)
+}
+
+func (c App) Hello(form_value string) revel.Result {
+  c.Validation.Required(form_value).Message("You Have to Input A Name!")
+  c.Validation.MinSize(form_value, 2).Message("Name should be more than 1 letters!")
+
+  if c.Validation.HasErrors() {
+    c.Validation.Keep()
+    c.FlashParams()
+    return c.Redirect(App.Index)
+  }
+
+  return c.Render(form_value)
 }
