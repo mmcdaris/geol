f6939e9fc623b096c1dba685a4bd8110958b5d41
mmcdaris <morganmcdaris@yahoo.com>
2014-03-01 22:34:08 -0800

    age boarders are display with green lines

--- public/js/timeline.js
+++ public/js/timeline.js
@@ -1,28 +1,52 @@
-var data = $.getJson("time.json")
+var max_height = 15000;
+var max_age = times[0]["ma"];
 
-
-var hght = 10000;
-view.element.height = hght;
-view.height = hght;
-
-var path = new Path();
-path.strokeColor = 'red';
-var start = new Point(200,200);
-path.moveTo(start);
-path.lineTo(start + [100, -40]);
-
-var center_circle = new Path.Circle({
-       center: view.center,
-       radius: 30,
-  strokeColor: 'black'
-});
-
-function onResize(event) {
-  center_circle.position = view.center;
+paper.install(window);
+window.onload = function() {
+  paper.setup('timeline');
+    setup();
+    draw_ages();
+  view.draw();
 }
 
-window.onscroll = scroll_resize;
-
-function scroll_resize() {
-  center_circle. = view.center;
+function setup () {
+  view.element.height = max_height;
+  view.height = max_height;
 }
+
+function horizontal(y) {
+  var path = new Path();
+  path.strokeColor = 'green';
+  path.strokeWidth = 5;
+  var start = new Point(0, y);
+  path.moveTo(start);
+  var end = new Point(view.element.width, y);
+  path.lineTo(end);
+  var circle = new Circle();
+}
+
+function line () {
+  var path = new Path();
+  path.strokeColor = 'black';
+  var start = new Point(100, 100);
+  path.moveTo(start);
+  path.lineTo(start.add([ 200, -50 ]));
+}
+
+function age_to_y_axis(time) {
+  // total height is max_height
+  // an element has ["ma"] key to get the years ago in millions.
+  // time/max_age = y-position/max_height
+  return max_height - (time/max_age * max_height);
+}
+
+function horizontals(element, index, array) {
+  var age = age_to_y_axis( element["ma"] );
+  horizontal(age);
+}
+
+function draw_ages() {
+  times.forEach(horizontals);
+}
+
+function
