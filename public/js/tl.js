729b3ac3241feb03a01b75c84ea948a6c65ec885
mmcdaris <morganmcdaris@yahoo.com>
2014-03-22 00:45:47 -0700

    renamed

--- public/js/tl.js
+++ public/js/tl.js
@@ -7,10 +7,13 @@ draw_ages();
 view.draw();
 summon_buddy(200, 200);
 
+function onResize(event) {
+  setup();
+}
+
 function summon_buddy (x, y) {
   buddy = new Path.Circle([x, y], 70);
   buddy.strokeWidth = 3;
-  buddy.smooth();
   buddy.strokeColor = "red";
   text = new PointText([x, y]);
   text.justification = 'center';
@@ -18,15 +21,15 @@ function summon_buddy (x, y) {
   text.content = ma(y);
 }
 
-function build_around_point(x, y, r) {
-  var shape = new Integer;
-}
-
 function onMouseMove(event) {
   //console.log(event.point);
   buddy.position = event.point;
   text.position = event.point;
   text.content = ma(event.point.y);
+}
+
+function onMouseUp(event) {
+  console.log(event.point);
 }
 
 function onFrame(event) {
@@ -39,15 +42,15 @@ function bg(color) {
 
 function setup () {
   view.viewSize.height = extra;
+  view.viewSize.width = window.innerWidth;
 }
 
 function horizontals(element, index, array) {
-  var age = age_to_y_axis( element["ma"] );
+  var age = age_to_y( element["ma"] );
   horizontal(age);
 }
 
 function horizontal(y) {
-  y += 100;
   var horiz = new Path();
   horiz.strokeColor = new Color( y_to_color(y), 0.7, 0.4);
   horiz.strokeWidth = 3;
@@ -58,24 +61,63 @@ function horizontal(y) {
 }
 
 function draw_ages() {
+  unit_to_boxes("supereon", 0);
+  unit_to_boxes("eon", 1);
+  unit_to_boxes("era", 2);
+  unit_to_boxes("period", 3);
+  unit_to_boxes("epoch", 4);
+  unit_to_boxes("age", 5);
   times.forEach(horizontals);
 }
 
-function y_ontimes() {
-  times.forEach();
+function find_edges(unit) {
+  var edges = {};
+  times.map (function (time) { if (time[unit] in edges) edges[time[unit]] = time.ma; else edges[time[unit]] = time.ma});
+  return edges;
 }
 
-function age_to_y_axis(time) {
-  return max_height - (time/max_age * max_height);
+function unit_to_boxes(unit, target) {
+  var width  = window.innerWidth/6;
+  var left   = target*width;
+  var top    = age_to_y(max_age);
+  var bottom;
+  var height;
+  var edges  = find_edges(unit);
+
+  for(element in edges) {
+    bottom = age_to_y(edges[element]);
+	height = bottom - top;
+	console.log(element);
+    var square = new Path.Rectangle([left,top,width,height])
+	if (element != null) {
+	  square.style = {
+	  fillColor: new Color(1, 0, 0),
+	  strokeColor: 'black',
+	  strokeWidth: 5 };
+      top = bottom;
+	} else {
+	  square.fillColor = "blue";
+	}
+  }
+}
+
+
+function age_to_y(time) {
+  var y = max_height - (time/max_age * max_height);
+  return y+100;
 }
 
 function y_to_age(y) {
-  y -= 100
+  y -= 100;
   return max_age - ( ( (y)/max_height ) * max_age );
 }
 
 function y_to_color(y) {
   return ((y/max_height) % 255 / 255);
+}
+
+function x_to_color(x) {
+  return ((x/window.innerWidth) % 255 / 255);
 }
 
 function ma(y) {
@@ -90,29 +132,4 @@ function ma(y) {
   else{
     return age + " Million Years Ago";
   }
-}
-
-
-// Timeline Object
-
-function Timeline() {
-    // has many timepoints
-    this.times = times;
-    this.times = name;
-    this.stats = function () {
-      console.log( this.name + " writes " + this.lang);
-    }
-}
-
-function Timepoint(age, eon, era, period, epoch) {
-  this.age    = age;
-  this.y      = age_to_y(age);
-  this.eon    = eon;
-  this.era    = era;
-  this.period = period;
-  this.epoch  = epoch;
-}
-
-function filter_for(k, v) {
-    times[v] = times.filter(function(x) { return x[k] == v});
 }
